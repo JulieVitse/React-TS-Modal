5eca8e4997ff52ce42f11d2647d0a521fcc91e4f
Julie V <julievitse91@gmail.com>
2023-03-18 17:14:50 +0100

    add: better prop types, default props, child component prop, showclose prop

--- src/lib/components/Modal/Modal.tsx
+++ src/lib/components/Modal/Modal.tsx
@@ -1,18 +1,34 @@
 import './modal.scss'
-//import btnClose from 'lib/assets/btn-close.png'
-//import { createPortal } from 'react-dom'
-interface modalProps {
+// Required props
+interface IModalRequiredProps {
   closeModal: () => void
   isOpen: boolean
-  escToClose: boolean,
-  clickOverlayClose: boolean,
-  textContent: string | null,
-  htmlContent: any,
-  modalTitle: string | null
-  selectedModal: any
 }
-
-export function Modal({ isOpen, closeModal, escToClose, clickOverlayClose, textContent, htmlContent, modalTitle, selectedModal }: modalProps) {
+// Optional props
+interface IModalOptionalProps {
+  escToClose?: boolean,
+  clickOverlayClose?: boolean,
+  textContent?: string,
+  htmlContent?: any,
+  modalTitle?: string
+  selectedModal?: any,
+  showClose?: boolean,
+  ChildComponent?: React.FC | null
+}
+// Modal props
+interface IModalProps extends IModalRequiredProps, IModalOptionalProps {}
+// Define default props
+const defaultProps: IModalOptionalProps = {
+  escToClose: true,
+  clickOverlayClose: true,
+  showClose: true,
+  textContent: '',
+  htmlContent: null,
+  modalTitle: '',
+  selectedModal: null,
+  ChildComponent: null
+}
+export function Modal({ isOpen, closeModal, escToClose, clickOverlayClose, showClose, textContent, htmlContent, modalTitle, selectedModal, ChildComponent }: IModalProps) {
 
   function handleEscClose(e: any) {
     if (e.key === 'Escape') {
@@ -22,27 +38,46 @@ export function Modal({ isOpen, closeModal, escToClose, clickOverlayClose, textC
   
   return isOpen && selectedModal ? (
     <>
-      <div
-        className="modal__overlay"
-        onClick={clickOverlayClose ? closeModal : undefined}
-      ></div>
-      <div className="modal">
-        <button className="modal__btn" onClick={closeModal}>
-          &#9587;
-        </button>
-        <div className="modal__content">
-          <h2 className="modal__content__title">{modalTitle}</h2>
-          <p className="modal__content__text">{textContent}</p>
-          <div
-            className="modal__content__html"
-            dangerouslySetInnerHTML={{ __html: htmlContent }}
-          />
+      <div className="wrapper">
+        <div
+          className="modal__overlay"
+          onClick={clickOverlayClose ? closeModal : undefined}
+        ></div>
+        <div className="modal">
+          {/* <button className="modal__btn" onClick={closeModal}>
+            &#9587;
+          </button> */}
+
+          <div className="modal__content">
+            
+            {modalTitle && (
+              <h2 className="modal__content__title">{modalTitle}</h2>
+            )}
+            {ChildComponent && (
+              <ChildComponent />
+            )}
+            {textContent && (
+              <p className="modal__content__text">{textContent}</p>
+            )}
+            {htmlContent && (
+              <div
+                className="modal__content__html"
+                dangerouslySetInnerHTML={{ __html: htmlContent }}
+              />
+            )}
+          </div>
+          {showClose && (
+            <button className="modal__btnClose" onClick={closeModal}>
+              Close
+            </button>
+          )}
         </div>
       </div>
-
       {escToClose
         ? window.addEventListener('keydown', handleEscClose)
         : window.removeEventListener('keydown', handleEscClose)}
     </>
   ) : null
 }
+
+Modal.defaultProps = defaultProps
