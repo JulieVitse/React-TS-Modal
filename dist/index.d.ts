e74f48df9576adb7212091a6bf1be80298e73d97
Julie V <julievitse91@gmail.com>
2023-05-08 14:08:07 +0200

    fix - add scss file to dist for publishing

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 import Modal from './components/Modal/Modal';
 import { useModal } from './components/Modal/useModal';
 import { IModalProps } from './types/modalTypes';
+import './components/Modal/modal.scss';
 export { Modal, useModal };
 export type { IModalProps };
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
 import Modal from './components/Modal/Modal';
 import { useModal } from './components/Modal/useModal';
+import './components/Modal/modal.scss';
 export { Modal, useModal };
